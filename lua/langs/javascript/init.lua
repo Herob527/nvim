75be49575b2e6c637a001b101a1c3babd74d4324
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-11-26 00:18:44 +0100

    Inline hints works!

--- lua/langs/javascript/init.lua
+++ lua/langs/javascript/init.lua
@@ -3,7 +3,40 @@ local M = {}
 M.treesitter = { "javascript", "typescript", "tsx", "jsdoc" }
 
 M.lspconfig = {
-	{ lsp = "tsserver" },
+	{
+		lsp = "tsserver",
+		settings = {
+			typescript = {
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			},
+			javascript = {
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			},
+		},
+		on_attach = function(client, bufnr)
+			if client.server_capabilities.inlayHintProvider then
+				vim.lsp.inlay_hint.enable(bufnr, true)
+			end
+		end,
+	},
 }
 
 M.mason = {}
@@ -26,8 +59,6 @@ M.null_ls = {
 	},
 	diagnostics = { { program = "eslint_d" } },
 	code_actions = { { program = "eslint_d" } },
-	rest = {
-
-	},
+	rest = {},
 }
 return M
