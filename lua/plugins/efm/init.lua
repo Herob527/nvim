6af131048a99e81b48bff1b623a2a1f8d2a5acf8
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-12-03 22:53:23 +0100

    Added more stuff to efm and working on groq

--- lua/plugins/efm/init.lua
+++ lua/plugins/efm/init.lua
@@ -1,49 +1,61 @@
 local M = {}
 
 M.init = function()
-  local eslint_d = require("efmls-configs.linters.eslint_d")
-  local prettier_d = require("efmls-configs.formatters.prettier_d")
-  local stylua = require("efmls-configs.formatters.stylua")
-  local rustfmt = require('efmls-configs.formatters.rustfmt')
+	local eslint_d = require("efmls-configs.linters.eslint_d")
+	local prettier_d = require("efmls-configs.formatters.prettier_d")
+	local stylua = require("efmls-configs.formatters.stylua")
+	local rustfmt = require("efmls-configs.formatters.rustfmt")
+	local black = require("efmls-configs.formatters.black")
+	local ruff_lint = require("efmls-configs.linters.ruff")
+	local json_lint = require("efmls-configs.linters.jq")
+	local yamllint = require("efmls-configs.linters.yamllint")
 
-  local languages = {
-    typescript = { eslint_d, prettier_d },
-    javascript = { eslint_d, prettier_d },
-    lua = { stylua },
-    rust = { rustfmt },
-  }
-  local efmls_config = {
-    filetypes = vim.tbl_keys(languages),
-    settings = {
-      rootMarkers = { ".git/" },
-      languages = languages,
-    },
-    init_options = {
-      documentFormatting = true,
-      documentRangeFormatting = true,
-    },
-  }
+	local languages = {
+		typescriptreact = { eslint_d, prettier_d },
+		javascriptreact = { eslint_d, prettier_d },
+		typescript = { eslint_d, prettier_d },
+		javascript = { eslint_d, prettier_d },
+		lua = { stylua },
+		rust = { rustfmt },
+		astro = { eslint_d, prettier_d },
+		html = { prettier_d },
+		vue = { prettier_d, eslint_d },
+		python = { black, ruff_lint },
+		json = { prettier_d, json_lint },
+		yaml = { prettier_d, yamllint },
+	}
+	local efmls_config = {
+		filetypes = vim.tbl_keys(languages),
+		settings = {
+			rootMarkers = { ".git/" },
+			languages = languages,
+		},
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+		},
+	}
 
-  require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
-    on_attach = require("lsp-format").on_attach,
-  }))
+	require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
+		on_attach = require("lsp-format").on_attach,
+	}))
 end
 
 M.config = {
-  "mattn/efm-langserver",
-  event = "VeryLazy",
-  build = "go install github.com/mattn/efm-langserver@latest",
-  dependencies = {
-    {
-      "creativenull/efmls-configs-nvim",
-      version = "v1.x.x", -- version is optional, but recommended
-      dependencies = {
-        "neovim/nvim-lspconfig",
-      },
-    },
-  },
-  config = function()
-    M.init()
-  end,
+	"mattn/efm-langserver",
+	event = "VeryLazy",
+	build = "go install github.com/mattn/efm-langserver@latest",
+	dependencies = {
+		{
+			"creativenull/efmls-configs-nvim",
+			version = "v1.x.x", -- version is optional, but recommended
+			dependencies = {
+				"neovim/nvim-lspconfig",
+			},
+		},
+	},
+	config = function()
+		M.init()
+	end,
 }
 return M
--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -10,7 +10,25 @@ M.init = function()
 			table.insert(ensure_installed, entry)
 		end
 	end
+	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
+	vim.cmd([[
+augroup filetypedetect
+  autocmd! BufRead,BufNewFile,BufEnter *.groq setfiletype groq
+augroup END
+]])
+	parser_config.groq = {
+		install_info = {
+			url = "~/Desktop/Projects/tree-sitter-groq", -- local path or git repo
+			files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+			-- optional entries:
+			branch = "main", -- default branch in case of git repo if different from master
+			generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+			requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+		},
+		filetype = "groq", -- if filetype does not match the parser name
+	}
+	vim.treesitter.language.register("groq", "groq") -- the someft filetype will use the python parser and queries.
 	require("nvim-treesitter.configs").setup({
 		-- A list of parser names, or "all" (the four listed parsers should always be installed)
 		ensure_installed = ensure_installed,
