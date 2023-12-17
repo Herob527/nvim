f0287b9b22ab47b0e013319083744e6531ce5dd3
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-12-17 22:00:05 +0100

    Added codeium support

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -1,12 +1,10 @@
 local M = {}
 
 M.init = function()
-	local coq = require("coq")
 	vim.g.coq_settings = {
 		limits = {
 			completion_auto_timeout = 0.2,
 		},
-		display = { pum = { fast_close = false } },
 		clients = {
 			lsp = {
 				enabled = true,
@@ -27,6 +25,22 @@ M.config = {
 	"ms-jpq/coq_nvim",
 	dependencies = {
 		"ms-jpq/coq.artifacts",
+		{
+			"ms-jpq/coq.thirdparty",
+			dependencies = {
+				"Exafunction/codeium.vim",
+				config = function()
+					vim.keymap.set("i", "<C-d>", function()
+						return vim.fn["codeium#Accept"]()
+					end, { expr = true })
+				end,
+			},
+			config = function()
+				require("coq_3p")({
+					{ src = "codeium", short_name = "COD" },
+				})
+			end,
+		},
 	},
 	event = "InsertCharPre",
 	config = function()
