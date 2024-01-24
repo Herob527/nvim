524f1b533d342517b7ad0db2871d5b587b841ff1
Herob527 <szymon.wrzos@onet.pl>
2024-01-24 23:25:57 +0100

    Removed redundant imports

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -1,51 +1,49 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazyinstalls = {
-	require("plugins.colorscheme.init").config,
-	require("plugins.treesitter.init").config,
-	require("plugins.nvim_tree.init").config,
-	"nvim-lua/plenary.nvim",
-	require("plugins.telescope.init").config,
-	-- require("plugins.fzf.init").config,
-	require("plugins.gitsigns.init").config,
-	require("plugins.lazygit.init").config,
-	require("plugins.wilder.init").config,
-	require("plugins.refactoring.init").config,
-	-- require("plugins.mason.dap").config,
-	require("plugins.cmp.init").config,
-	require("plugins.lualine.init").config,
-	require("plugins.bufferline.init").config,
-	require("plugins.trouble.init").config,
-	require("plugins.leap.init").config,
-	require("plugins.spectre.init").config,
-	require("plugins.code_action_menu.init").config,
-	require("plugins.nvim_comment.init").config,
-	require("plugins.which_key.init").config,
-	{
-		"j-hui/fidget.nvim",
-		event = "LspAttach",
-		opts = {
-			-- options
-		},
-	},
-	-- mason,
+  require("plugins.colorscheme.init").config,
+  require("plugins.treesitter.init").config,
+  require("plugins.nvim_tree.init").config,
+  "nvim-lua/plenary.nvim",
+  require("plugins.telescope.init").config,
+  require("plugins.gitsigns.init").config,
+  require("plugins.lazygit.init").config,
+  require("plugins.wilder.init").config,
+  require("plugins.refactoring.init").config,
+  require("plugins.cmp.init").config,
+  require("plugins.lualine.init").config,
+  require("plugins.bufferline.init").config,
+  require("plugins.trouble.init").config,
+  require("plugins.leap.init").config,
+  require("plugins.spectre.init").config,
+  require("plugins.code_action_menu.init").config,
+  require("plugins.nvim_comment.init").config,
+  require("plugins.which_key.init").config,
+  {
+    "j-hui/fidget.nvim",
+    event = "LspAttach",
+    opts = {
+      -- options
+    },
+  },
+  -- mason,
 }
 
 local opts = {
-	defaults = {
-		lazy = true,
-	},
+  defaults = {
+    lazy = true,
+  },
 }
 
 require("lazy").setup(lazyinstalls, opts)
