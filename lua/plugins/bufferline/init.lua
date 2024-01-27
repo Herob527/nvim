94efaf079772bff2631fec59ae6ab86c96cab4b5
Herob527 <szymon.wrzos@onet.pl>
2024-01-27 13:28:25 +0100

    Formatting

--- lua/plugins/bufferline/init.lua
+++ lua/plugins/bufferline/init.lua
@@ -1,51 +1,51 @@
 local M = {}
 
 M.init = function()
-	local status_ok, bufferline = pcall(require, "bufferline")
-	if not status_ok then
-		return
-	end
-	local map = vim.api.nvim_set_keymap
+  local status_ok, bufferline = pcall(require, "bufferline")
+  if not status_ok then
+    return
+  end
+  local map = vim.api.nvim_set_keymap
 
-	local diagnostics_indicator = function(count, level, diagnostics_dict, context)
-		local icon = level:match("error") and " " or " "
-		return " " .. icon .. count
-	end
+  local diagnostics_indicator = function(count, level, diagnostics_dict, context)
+    local icon = level:match("error") and " " or " "
+    return " " .. icon .. count
+  end
 
-	map("n", "<C-l>", "<cmd>:BufferLineCycleNext<cr>", { desc = "Next Tab" })
-	map("n", "<C-k>", "<cmd>:BufferLineCyclePrev<cr>", { desc = "Previous Tab" })
-	map("n", "<leader>;", "<cmd>:BufferLineCloseRight<cr>", { desc = "Close tabs to the right" })
-	map("n", "<leader>k", "<cmd>:BufferLineCloseLeft<cr>", { desc = "Close tabs to the left" })
+  map("n", "<C-l>", "<cmd>:BufferLineCycleNext<cr>", { desc = "Next Tab" })
+  map("n", "<C-k>", "<cmd>:BufferLineCyclePrev<cr>", { desc = "Previous Tab" })
+  map("n", "<leader>;", "<cmd>:BufferLineCloseRight<cr>", { desc = "Close tabs to the right" })
+  map("n", "<leader>k", "<cmd>:BufferLineCloseLeft<cr>", { desc = "Close tabs to the left" })
 
-	bufferline.setup({
-		options = {
-			diagnostics = "nvim_lsp",
-			diagnostics_indicator = diagnostics_indicator,
-		},
-	})
+  bufferline.setup({
+    options = {
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = diagnostics_indicator,
+    },
+  })
 end
 
 M.config = {
-	"akinsho/bufferline.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-web-devicons").setup({
-				override_by_extension = {
-					["astro"] = {
-						icon = "󰑣",
-						color = "#FF5733",
-						name = "astro",
-					},
-				},
-			})
-		end,
-	},
-	event = "BufAdd",
-	config = function()
-		M.init()
-	end,
+  "akinsho/bufferline.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-web-devicons").setup({
+        override_by_extension = {
+          ["astro"] = {
+            icon = "󰑣",
+            color = "#FF5733",
+            name = "astro",
+          },
+        },
+      })
+    end,
+  },
+  event = "BufAdd",
+  config = function()
+    M.init()
+  end,
 }
 
 return M
