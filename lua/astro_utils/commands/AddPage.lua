df63dc5808c5695b6aa6ed28070c7d7354b9ec45
Herob527 <szymon.wrzos@onet.pl>
2024-02-11 16:10:25 +0100

    Handle case if file is found

--- lua/astro_utils/commands/AddPage.lua
+++ lua/astro_utils/commands/AddPage.lua
@@ -9,6 +9,7 @@ interface Props {
 ---]],
 }
 
+--- @param name string
 local function create_page(name)
 	if name == nil or name == "" then
 		error("No name provided")
@@ -22,6 +23,19 @@ local function create_page(name)
 	if not endswith(page_path, ".astro") then
 		page_path = page_path .. ".astro"
 	end
+	if vim.fn.findfile(page_path) then
+		vim.ui.select({ "yes", "no" }, { prompt = "File exists. Overwrite?" }, function(choice)
+			if choice == "yes" then
+				print("\nOverwriting " .. page_path)
+				local dummy_content = templates[1]
+				vim.fn.delete(page_path)
+				vim.fn.writefile(lines(dummy_content), page_path, "a")
+			else
+				print("\nFile " .. page_path .. " not overwritten")
+			end
+		end)
+		return
+	end
 
 	local dummy_content = templates[1]
 	vim.fn.delete(page_path)
@@ -29,21 +43,21 @@ local function create_page(name)
 end
 
 M.func = function(opts)
+	local split_args = require("astro_utils.utils").split_args
 	local cwd = vim.fn.getcwd()
 	local astro_config_path = cwd .. "/astro.config.mjs"
-
-	local utils = require("astro_utils.utils")
-	local split_args = utils.split_args
 	local args = split_args(opts.args)
+
 	if #args > 1 then
 		error("Too many arguments. This function accepts one")
 		return
 	end
 
 	if vim.fn.filereadable(astro_config_path) == 0 then
+		print("No astro config found")
 		return
 	end
-	print(args[1])
+
 	if #args == 1 and args[1] ~= "" then
 		create_page(args[1])
 	else
