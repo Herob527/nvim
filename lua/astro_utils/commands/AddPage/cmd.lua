10a2354b170d9905372b8c1de6711b2ae52f6628
Herob527 <szymon.wrzos@onet.pl>
2024-02-17 23:52:25 +0100

    Added on_exist_handler

--- lua/astro_utils/commands/AddPage/cmd.lua
+++ lua/astro_utils/commands/AddPage/cmd.lua
@@ -1,12 +1,34 @@
 local M = {}
 
+local on_exist_handler = function(page_path)
+	local value = false
+	vim.ui.select({ "yes", "no" }, {
+		prompt = "File exists.",
+		format_item = function(item)
+			if item == "yes" then
+				return "Overwrite file"
+			else
+				return "Cancel"
+			end
+		end,
+	}, function(choice)
+		if choice == "yes" then
+			value = true
+			vim.print("File " .. page_path .. " overwritten")
+		else
+			vim.print("File " .. page_path .. " not overwritten")
+		end
+	end)
+	return value
+end
+
 M.launch_cmd = function(args)
 	local common = require("astro_utils.commands.AddPage.common")
 	if #args == 1 and args[1] ~= "" then
-		common.create_page(args[1])
+		common.create_page(args[1], on_exist_handler)
 	else
 		vim.ui.input({ prompt = "Page name: " }, function(page_name)
-			common.create_page(page_name)
+			common.create_page(page_name, on_exist_handler)
 		end)
 	end
 end
