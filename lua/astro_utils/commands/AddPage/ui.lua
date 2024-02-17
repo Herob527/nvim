be9e7d6fdd5c5e601f6aa89110ffa6355fed4fba
Herob527 <szymon.wrzos@onet.pl>
2024-02-18 00:24:15 +0100

    Formatting and made it work

--- lua/astro_utils/commands/AddPage/ui.lua
+++ lua/astro_utils/commands/AddPage/ui.lua
@@ -1,84 +1,97 @@
 local M = {}
+
+
 M.launch_ui = function()
-	local Input = require("nui.input")
-	local Layout = require("nui.layout")
-	local Popup = require("nui.popup")
+  local commons = require("astro_utils.commands.AddPage.common")
+  local utils = require("astro_utils.utils")
+  local path_exists = utils.path_exists
+  local endswith = utils.endswith
+  local Input = require("nui.input")
+  local Layout = require("nui.layout")
+  local Popup = require("nui.popup")
 
-	local popup_two = Popup({
-		focusable = false,
-		buf_options = {
-			modifiable = true,
-			readonly = true,
-		},
-		zindex = 50,
-		mode = "action",
-		relative = "editor",
-		border = {
-			padding = {
-				top = 1,
-				left = 2,
-				right = 1,
-			},
-			style = "rounded",
-			text = {
-				top = " Status ",
-				top_align = "left",
-			},
-		},
-	})
-	local input = Input({
-		position = "50%",
-		size = {
-			width = 32,
-		},
-		border = {
-			style = "single",
-			text = {
-				top = "Name file to create / replace",
-				top_align = "center",
-			},
-		},
-		win_options = {
-			winhighlight = "Normal:Normal,FloatBorder:Normal",
-		},
-	}, {
-		prompt = "> ",
-		default_value = "",
-		on_close = function()
-			print("Input Closed!")
-		end,
-		on_submit = function(value)
-			print("Input Submitted: " .. value)
-		end,
-		on_change = function(value)
-			if value == "" then
-				return
-			end
-			vim.schedule(function()
-				local ok, data = pcall(vim.api.nvim_buf_set_lines, popup_two.bufnr, 0, 2, false, { value })
+  local cwd = vim.fn.getcwd()
+  local pages_path = cwd .. "/src/pages/"
+  local popup_two = Popup({
+    focusable = false,
+    buf_options = {
+      modifiable = true,
+      readonly = true,
+    },
+    zindex = 50,
+    mode = "action",
+    relative = "editor",
+    border = {
+      padding = {
+        top = 1,
+        left = 2,
+        right = 1,
+      },
+      style = "rounded",
+      text = {
+        top = " Status ",
+        top_align = "left",
+      },
+    },
+  })
+  local input = Input({
+    position = "50%",
+    size = {
+      width = 32,
+    },
+    border = {
+      style = "single",
+      text = {
+        top = "Name file to create / replace",
+        top_align = "center",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal,FloatBorder:Normal",
+    },
+  }, {
+    prompt = "> ",
+    default_value = "",
+    on_close = function()
+      print("Input Closed!")
+    end,
+    on_submit = function(value)
+      commons.create_page(value)
+    end,
+    on_change = function(value)
+      if value == "" then
+        return
+      end
+      vim.schedule(function()
+        vim.print(value)
+        local result_value = value .. (endswith(value, ".astro") and "" or ".astro")
 
-				vim.print("Value changed: " .. value, ok, data)
-			end)
-		end,
-	})
-	local layout = Layout(
-		{
-			position = "50%",
-			size = {
-				width = 80,
-				height = "40%",
-			},
-		},
-		Layout.Box({
-			Layout.Box(input, { grow = 1 }),
-			Layout.Box(popup_two, { grow = 4 }),
-		}, { dir = "col" })
-	)
-	-- mount/open the component
-	layout:mount()
-	input:on("BufLeave", function()
-		layout:unmount()
-	end)
+        local page_path = pages_path .. result_value
+        local file_exists = path_exists(page_path)
+        local error_msg = file_exists and "File already exists. Accepting input will overwrite it" or ""
+        vim.api.nvim_buf_set_lines(popup_two.bufnr, 0, -1, false,
+          { "Created path: /src/pages/" .. result_value, "", error_msg })
+      end)
+    end,
+  })
+  local layout = Layout(
+    {
+      position = "50%",
+      size = {
+        width = 80,
+        height = "40%",
+      },
+    },
+    Layout.Box({
+      Layout.Box(input, { grow = 1 }),
+      Layout.Box(popup_two, { grow = 5 }),
+    }, { dir = "col" })
+  )
+  -- mount/open the component
+  layout:mount()
+  input:on("BufLeave", function()
+    layout:unmount()
+  end)
 end
 
 package.path = "AddPage.ui"
