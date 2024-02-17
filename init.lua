dfde3fcf4d4d38233499760bd578757baaae81e2
Herob527 <szymon.wrzos@onet.pl>
2024-02-17 23:53:04 +0100

    Removed redundant plugin and fix for terraform being tf file on creation

--- init.lua
+++ init.lua
@@ -4,3 +4,11 @@ require("plugins_entry")
 
 require("key_bindings")
 require("opts")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		if vim.bo.filetype == "tf" then
+			vim.bo.filetype = "terraform"
+		end
+	end,
+})
