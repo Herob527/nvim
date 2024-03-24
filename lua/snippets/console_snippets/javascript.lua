7c2d23c989612971749fab67d80d5462655b9c14
Herob527 <szymon.wrzos@onet.pl>
2024-03-24 22:48:57 +0100

    Fixed queries

--- lua/snippets/console_snippets/javascript.lua
+++ lua/snippets/console_snippets/javascript.lua
@@ -1,14 +1,13 @@
 local create_snippet = require("snippets.create_printing_snippet")
 
 local query = [[
-(field_definition (property_identifier) @name (function) (#set! "fetch_last" "true"))
+(function_declaration (identifier) @name (#set! "fetch_last" "true"))
 (field_definition (property_identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (function_declaration (identifier) @name)
-(variable_declarator (identifier) @name (function) (#set! "fetch_last" "true"))
+(variable_declarator (identifier) @name (function_expression) (#set! "fetch_last" "true"))
 (variable_declarator (identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (method_definition (private_property_identifier) @name)
 (method_definition (property_identifier) @name)
-(pair (property_identifier) @key)
 ]]
 
 local breakpoints = {
@@ -16,7 +15,6 @@ local breakpoints = {
 	"function_declaration",
 	"variable_declarator",
 	"method_definition",
-	"pair",
 }
 
 local format = [[console.log("[{function_name}]", {var})]]
