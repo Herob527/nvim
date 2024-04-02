2dfa1cc3f3346fdc790d69e84402f6a1f677d63b
Herob527 <szymon.wrzos@onet.pl>
2024-04-02 23:38:37 +0200

    Adjusted to more reproducible approach

--- lua/langs/vue/init.lua
+++ lua/langs/vue/init.lua
@@ -3,22 +3,39 @@ local M = {}
 M.treesitter = { "vue" }
 
 M.lspconfig = {
-  { lsp = "volar" },
-  { lsp = "eslint" },
+	{
+		lsp = "tsserver",
+		init_options = {
+			plugins = {
+				{
+					name = "@vue/typescript-plugin",
+					location =   vim.fn.stdpath("data") .. "/lazy".. "/packages" .. "/node_modules/@vue/typescript-plugin",
+					languages = { "javascript", "typescript", "vue" },
+				},
+			},
+		},
+		filetypes = {
+			"javascript",
+			"typescript",
+			"vue",
+		},
+	},
+	{ lsp = "volar" },
+	{ lsp = "eslint" },
 }
 
 M.mason = {}
 
 M.mason.lspconfig = {
-  "volar",
-  "eslint",
+	"volar",
+	"eslint",
 }
 
 M.mason.null_ls = {
-  "prettierd",
+	"prettierd",
 }
 
 M.null_ls = {
-  formatting = { { program = "prettierd", with = { filetypes = { "vue" } } } },
+	formatting = { { program = "prettierd", with = { filetypes = { "vue" } } } },
 }
 return M
