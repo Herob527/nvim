bc9e905176d32f76ce6496992a8a92f70b3edb5a
Herob527 <szymon.wrzos@onet.pl>
2024-04-20 21:22:07 +0200

    Fixed issue when snippet didn't work in async expressions

--- lua/snippets/console_snippets/typescriptreact.lua
+++ lua/snippets/console_snippets/typescriptreact.lua
@@ -2,13 +2,14 @@ local create_snippet = require("snippets.create_printing_snippet")
 
 local query = [[
 (function_declaration (identifier) @name (#set! "fetch_last" "true"))
-(variable_declarator (identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (public_field_definition (private_property_identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (public_field_definition (property_identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (method_definition (private_property_identifier) @name)
 (method_definition (property_identifier) @name)
 (function_declaration (identifier) @name)
+(variable_declarator (identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (variable_declarator (identifier) @name (function_expression) (#set! "fetch_last" "true"))
+(variable_declarator (identifier) @name (parenthesized_expression) (#set! "fetch_last" "true"))
 ]]
 
 local breakpoints = {
