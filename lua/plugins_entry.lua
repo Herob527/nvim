bfd666c278a7e7d2697a194b4a88c24596b389bd
Herob527 <szymon.wrzos@onet.pl>
2024-06-09 15:06:35 +0200

    Added nvim vtsls plugin

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -45,6 +45,14 @@ local lazyinstalls = {
 		"kosayoda/nvim-lightbulb",
 		event = "LspAttach",
 	},
+	{
+		"yioneko/nvim-vtsls",
+		event = "LspAttach",
+		config = function()
+			require("lspconfig.configs").vtsls = require("vtsls").lspconfig
+		end,
+		dependencies = { "neovim/nvim-lspconfig" },
+	},
 	mason,
 }
 
