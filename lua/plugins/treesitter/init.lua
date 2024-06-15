0c864822ce0eb5817e8f0415fb72121d9fbadc7e
Herob527 <szymon.wrzos@onet.pl>
2024-06-15 23:13:14 +0200

    Added context

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -22,7 +22,16 @@ M.init = function()
 		auto_install = true,
 		autotag = {
 			enable = true,
-			filetypes = { "html", "xml", "javascript", "javascriptreact", "astro", "typescript", "typescriptreact", "vue" },
+			filetypes = {
+				"html",
+				"xml",
+				"javascript",
+				"javascriptreact",
+				"astro",
+				"typescript",
+				"typescriptreact",
+				"vue",
+			},
 		},
 		playground = {
 			enable = true,
@@ -42,6 +51,19 @@ M.init = function()
 				show_help = "?",
 			},
 		},
+		require("treesitter-context").setup({
+			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+			max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+			line_numbers = true,
+			multiline_threshold = 20, -- Maximum number of lines to show for a single context
+			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+			mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
+			-- Separator between context and content. Should be a single character string, like '-'.
+			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+			separator = nil,
+			zindex = 20, -- The Z-index of the context window
+			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+		}),
 		query_linter = {
 			enable = true,
 			use_virtual_text = true,
@@ -62,6 +84,7 @@ M.config = {
 		"windwp/nvim-ts-autotag",
 		"nvim-treesitter/playground",
 		"nvim-treesitter/nvim-tree-docs",
+		"nvim-treesitter/nvim-treesitter-context",
 	},
 	config = function()
 		M.init()
