0dc47fec0ac90e8a916eec9d49786d3225edd651
Herob527 <szymon.wrzos@onet.pl>
2024-06-16 20:06:16 +0200

    Removed redudant var

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -1,5 +1,3 @@
-local opts = { noremap = true, silent = true }
-
 vim.keymap.set({ "n" }, "<leader>ha", vim.lsp.buf.hover, { desc = "Trigger [h]over [a]ction" })
 vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.format, { desc = "Trigger [L]SP formatting" })
 
