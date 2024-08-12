c2a223c19cc919cfa5c5afb3df573c23b73f747c
Herob527 <szymon.wrzos@onet.pl>
2024-08-12 18:38:44 +0200

    Updated docs

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -4,4 +4,4 @@ vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.format, { desc = "Trigger [L]SP
 vim.keymap.set({ "n" }, "<leader>ef", function()
 	vim.cmd("silent exec ':LspRestart'")
 	vim.diagnostic.reset()
-end, { desc = "Reset eslint_d and diagnostics", silent = true })
+end, { desc = "Reset eslint and diagnostics", silent = true })
