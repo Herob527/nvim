d92d9fa23c5f8f1542db589dd69b304dbb59c35e
Herob527 <szymon.wrzos@onet.pl>
2024-09-03 10:00:07 +0200

    Updated which-key config

--- lua/plugins/which_key/init.lua
+++ lua/plugins/which_key/init.lua
@@ -12,16 +12,15 @@ M.init = function()
 			z = true, -- bindings for folds, spelling and others prefixed with z
 			g = true, -- bindings for prefixed with g
 		},
-		motions = {
-			count = true,
-		},
-		window = {
-			border = "single", -- none, single, double, shadow
-			position = "bottom", -- bottom, top
-			margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-			padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
-			winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-			zindex = 1000, -- positive value to position WhichKey above other floating windows.
+		opts = {
+			win = {
+				border = "single", -- none, single, double, shadow
+				position = "bottom", -- bottom, top
+				margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+				padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
+				winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+				zindex = 1000, -- positive value to position WhichKey above other floating windows.
+			},
 		},
 		layout = {
 			height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -36,6 +35,7 @@ M.config = {
 	"folke/which-key.nvim",
 	cmd = "WhichKey",
 	keys = { { "<leader>" }, { '"' }, { "`" }, { "d" }, { "z" }, { "c" }, { "v" }, { "g" }, { "y" }, { "!" } },
+	dependencies = { "echasnovski/mini.icons" },
 	config = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
