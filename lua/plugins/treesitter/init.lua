e3a2d554e1f523d71370448c8182c670a05f0e33
Herob527 <szymon.wrzos@onet.pl>
2024-09-08 16:08:46 +0200

    Added autotag support

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -20,19 +20,6 @@ M.init = function()
 		sync_install = true,
 		ignore_install = {},
 		auto_install = true,
-		autotag = {
-			enable = true,
-			filetypes = {
-				"html",
-				"xml",
-				"javascript",
-				"javascriptreact",
-				"astro",
-				"typescript",
-				"typescriptreact",
-				"vue",
-			},
-		},
 		playground = {
 			enable = true,
 			disable = {},
@@ -72,6 +59,22 @@ M.init = function()
 		tree_docs = { enable = true },
 	})
 	vim.treesitter.language.register("markdown", "mdx")
+	require("nvim-ts-autotag").setup({
+		opts = {
+			-- Defaults
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
+		},
+		-- Also override individual filetype configs, these take priority.
+		-- Empty by default, useful if one of the "opts" global settings
+		-- doesn't work well in a specific filetype
+		per_filetype = {
+			["html"] = {
+				enable_close = false,
+			},
+		},
+	})
 end
 
 M.config = {
