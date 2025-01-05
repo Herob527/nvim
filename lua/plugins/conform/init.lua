aebe766a062d3371928a33cc2dcdb16047f8bc7e
Herob527 <szymon.wrzos@onet.pl>
2025-01-05 15:41:45 +0100

    Formatting + added mason

--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -1,40 +1,51 @@
 local M = {}
 
 M.init = function()
-  local langs = require("utils.langs_table")
-  local test = vim.iter(langs)
-      :map(function(lang, content)
-        if content.conform == nil then
-          return nil
-        else
-          return { [lang] = content.conform }
-        end
-      end)
-      :filter(function(data) return data ~= nil end)
-      :totable()
+	local langs = require("utils.langs_table")
+	local test = vim.iter(langs)
+		:map(function(lang, content)
+			if content.conform == nil then
+				return nil
+			else
+				return { [lang] = content.conform }
+			end
+		end)
+		:filter(function(data)
+			return data ~= nil
+		end)
+		:totable()
 
-  local formatters = {}
+	local formatters = {}
 
-  for k in pairs(test) do
-    formatters = vim.tbl_extend('force', formatters, test[k])
-  end
+	for k in pairs(test) do
+		formatters = vim.tbl_extend("force", formatters, test[k])
+	end
 
-  require("conform").setup({
-    format_after_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
-    formatters_by_ft = formatters,
-  })
+	require("conform").setup({
+		format_after_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = formatters,
+	})
 end
 
 M.config = {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  config = function()
-    M.init()
-  end
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	config = function()
+		M.init()
+	end,
+	dependencies = {
+		"zapling/mason-conform.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("mason-conform").setup({
+				ensure_installed = { "black", "ruff" },
+			})
+		end,
+	},
 }
 
 return M
