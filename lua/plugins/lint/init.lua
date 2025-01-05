982186cbd2293ad96f5b76cd500787a5c1005bb5
Herob527 <szymon.wrzos@onet.pl>
2025-01-05 15:44:36 +0100

    Formatting and added mason nvim lint

--- lua/plugins/lint/init.lua
+++ lua/plugins/lint/init.lua
@@ -1,40 +1,50 @@
 local M = {}
 
 M.init = function()
-  local langs = require("utils.langs_table")
-  local test = vim.iter(langs)
-      :map(function(lang, content)
-        if content.linter == nil then
-          return nil
-        else
-          return { [lang] = content.linter }
-        end
-      end)
-      :filter(function(data) return data ~= nil end)
-      :totable()
+	local langs = require("utils.langs_table")
+	local test = vim.iter(langs)
+		:map(function(lang, content)
+			if content.linter == nil then
+				return nil
+			else
+				return { [lang] = content.linter }
+			end
+		end)
+		:filter(function(data)
+			return data ~= nil
+		end)
+		:totable()
 
-  local linters = {}
+	local linters = {}
 
-  for k in pairs(test) do
-    linters = vim.tbl_extend('force', linters, test[k])
-  end
-  require('lint').linters_by_ft = linters
+	for k in pairs(test) do
+		linters = vim.tbl_extend("force", linters, test[k])
+	end
+	require("lint").linters_by_ft = linters
 
-  vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
-    callback = function()
-      -- try_lint without arguments runs the linters defined in `linters_by_ft`
-      -- for the current filetype
-      require("lint").try_lint()
-    end,
-  })
+	vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+		callback = function()
+			-- try_lint without arguments runs the linters defined in `linters_by_ft`
+			-- for the current filetype
+			require("lint").try_lint()
+		end,
+	})
 end
 
 M.config = {
-  "mfussenegger/nvim-lint",
-  event = "VeryLazy",
-  config = function()
-    M.init()
-  end
+	"mfussenegger/nvim-lint",
+	event = "VeryLazy",
+	config = function()
+		M.init()
+	end,
+	dependencies = {
+		{
+			"rshkarin/mason-nvim-lint",
+			config = function()
+				require("mason-nvim-lint").setup()
+			end,
+		},
+	},
 }
 
 return M
