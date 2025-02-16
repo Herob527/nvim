30d78f1ef2904a4b1e82cb3709b35e8fc5ee1095
Herob527 <szymon.wrzos@onet.pl>
2025-02-16 19:05:52 +0100

    Updated resetting call

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -2,6 +2,7 @@ vim.keymap.set({ "n" }, "<leader>ha", vim.lsp.buf.hover, { desc = "Trigger [h]ov
 vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.format, { desc = "Trigger [L]SP formatting" })
 
 vim.keymap.set({ "n" }, "<leader>ef", function()
+	vim.cmd("silent exec ':VtsExec restart_tsserver'")
 	vim.cmd("silent exec ':LspRestart'")
 	vim.diagnostic.reset()
-end, { desc = "Reset eslint and diagnostics", silent = true })
+end, { desc = "Reset LSP and diagnostics", silent = true })
