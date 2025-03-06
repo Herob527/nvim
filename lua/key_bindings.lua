908d34d5a035fb1b8570eea6becb09935421db5c
Herob527 <szymon.wrzos@onet.pl>
2025-03-06 14:10:59 +0100

    Before executing VtsExec, check if command is there

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -2,7 +2,9 @@ vim.keymap.set({ "n" }, "<leader>ha", vim.lsp.buf.hover, { desc = "Trigger [h]ov
 vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.format, { desc = "Trigger [L]SP formatting" })
 
 vim.keymap.set({ "n" }, "<leader>ef", function()
-	vim.cmd("silent exec ':VtsExec restart_tsserver'")
+	if not vim.fn.exists(":VtsExec") == 2 then
+		vim.cmd("silent exec ':VtsExec restart_tsserver'")
+	end
 	vim.cmd("silent exec ':LspRestart'")
 	vim.diagnostic.reset()
 end, { desc = "Reset LSP and diagnostics", silent = true })
