10d8f192caee84e9a4e84fb83b4aab44b00ad9bc
Herob527 <szymon.wrzos@onet.pl>
2025-03-11 16:23:30 +0100

    Working on support for conditional addition of conform formatters

--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -2,11 +2,29 @@ local M = {}
 
 M.init = function()
 	local langs = require("utils.langs_table")
+	local project_marker = { ".git" }
+	local project_root = vim.fs.root(0, project_marker)
 	local test = vim.iter(langs)
 		:map(function(lang, content)
-			if content.conform == nil then
+			if content.conform == nil or vim.tbl_isempty(content.conform) then
 				return nil
+			-- TODO: Make this flexible allowing more formatters follow this
+			elseif type(content.conform[1]) == "table" then
+				local has_requires = vim.iter(content.conform[1].requires)
+					:map(function(item)
+						return project_root .. "/" .. item
+					end)
+					:any(function(item)
+						return vim.fn.filereadable(item) == 1
+					end)
+
+				if has_requires then
+					return { [lang] = { content.conform[1].name } }
+				else
+					return nil
+				end
 			else
+				-- vim.print(content.conform)
 				return { [lang] = content.conform }
 			end
 		end)
@@ -20,6 +38,8 @@ M.init = function()
 	for k in pairs(test) do
 		formatters = vim.tbl_extend("force", formatters, test[k])
 	end
+
+	vim.print(formatters)
 
 	require("conform").setup({
 		format_after_save = {
@@ -38,11 +58,13 @@ M.config = {
 		M.init()
 	end,
 	dependencies = {
-		"zapling/mason-conform.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("mason-conform").setup()
-		end,
+		{
+			"zapling/mason-conform.nvim",
+			event = "VeryLazy",
+			config = function()
+				require("mason-conform").setup()
+			end,
+		},
 	},
 }
 
