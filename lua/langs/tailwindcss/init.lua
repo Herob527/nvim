870ac0ee5479658490d77badb02cfd60ff7089d9
Herob527 <szymon.wrzos@onet.pl>
2025-04-19 08:44:02 +0200

    Added nuxt js workaround for tailwind

--- lua/langs/tailwindcss/init.lua
+++ lua/langs/tailwindcss/init.lua
@@ -5,11 +5,23 @@ M.treesitter = {}
 M.lspconfig = {
 	{
 		lsp = "tailwindcss",
-		settings = {
-			tailwindCSS = {
-				classAttributes = { "class-name", "class", "className", "class:list" },
-			},
-		},
+		settings = function()
+			local nuxt_tailwind_path = "assets/css/main.css"
+
+			local nuxt_config = { "nuxt.config.ts" }
+			local project_root = vim.fs.root(0, nuxt_config)
+			local apply_nuxt_tailwind = project_root ~= nil
+					and (vim.fn.filereadable(project_root .. "/" .. nuxt_tailwind_path) == 1)
+				or false
+			return {
+				tailwindCSS = {
+					classAttributes = { "class-name", "class", "className", "class:list" },
+					experimental = {
+						configFile = apply_nuxt_tailwind and nuxt_tailwind_path or nil,
+					},
+				},
+			}
+		end,
 	},
 }
 
