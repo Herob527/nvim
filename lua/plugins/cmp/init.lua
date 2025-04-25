a3ceb5ef7e9a75a811537958935a4744f3b2d16b
Herob527 <szymon.wrzos@onet.pl>
2025-04-25 23:30:08 +0200

    Formattings

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -43,27 +43,52 @@ M.config = {
 	lazy = false,
 	version = "1.*",
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = { preset = "enter" },
 
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+
+			highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
+			use_nvim_cmp_as_default = false,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
+			kind_icons = {
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "󰖷",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
 		},
 		signature = { enabled = true },
-		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
 			list = { selection = { preselect = true, auto_insert = false } },
 			ghost_text = {
@@ -90,9 +115,6 @@ M.config = {
 								return icon .. ctx.icon_gap
 							end,
 
-							-- Optionally, use the highlight groups from nvim-web-devicons
-							-- You can also add the same function for `kind.highlight` if you want to
-							-- keep the highlight groups in sync with the icons.
 							highlight = function(ctx)
 								local hl = ctx.kind_hl
 								if vim.tbl_contains({ "Path" }, ctx.source_name) then
@@ -110,7 +132,6 @@ M.config = {
 			documentation = { auto_show = true },
 		},
 		cmdline = {
-			-- recommended, as the default keymap will only show and select the next item
 			keymap = {
 				preset = "default",
 				["<Up>"] = { "select_prev", "fallback" },
@@ -120,25 +141,16 @@ M.config = {
 				menu = {
 					auto_show = function(ctx)
 						return vim.fn.getcmdtype() == ":"
-						-- enable for inputs as well, with:
-						-- or vim.fn.getcmdtype() == '@'
 					end,
 				},
 			},
 		},
 		snippets = { preset = "luasnip" },
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
 			providers = { ripgrep = ripgrep },
 		},
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust" },
 	},
 	opts_extend = { "sources.default" },
