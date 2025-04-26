65c9156e983a24bb547db00de370e0336d170513
Herob527 <szymon.wrzos@onet.pl>
2025-04-26 15:02:21 +0200

    Added notifying for not working ai service

--- lua/utils/ai-utils.lua
+++ lua/utils/ai-utils.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.OPEN_AI_ENDPOINT = "http://localhost:1234/v1"
 
+local notified = false
 --- @param url string | nil
 M.is_operational = function(url)
 	local response = vim.fn.systemlist({
@@ -14,6 +15,13 @@ M.is_operational = function(url)
 		url or M.OPEN_AI_ENDPOINT,
 	})[1]
 	local is_running = tonumber(response) == 200
+	if not notified and not is_running then
+		vim.notify(
+			"AI service responded with status " .. tonumber(response) .. ". AI plugins will be disabled.",
+			vim.log.levels.INFO
+		)
+		notified = true
+	end
 
 	return is_running
 end
