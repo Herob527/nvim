a661715ce1193481bd94f1e95788b2a0048dae95
Herob527 <szymon.wrzos@onet.pl>
2025-04-26 14:29:15 +0200

    Set default flavour

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -2,11 +2,7 @@ local M = {}
 
 M.init = function()
 	require("catppuccin").setup({
-		flavour = "auto", -- latte, frappe, macchiato, mocha
-		background = { -- :h background
-			light = "latte",
-			dark = "mocha",
-		},
+		flavour = "frappe", -- latte, frappe, macchiato, mocha
 		transparent_background = false, -- disables setting the background color.
 		show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 		term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
