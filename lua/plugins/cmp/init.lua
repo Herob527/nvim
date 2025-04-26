c1afcb7a14fb5ac05b1a9f466b83166e2a83f1c2
Herob527 <szymon.wrzos@onet.pl>
2025-04-26 14:29:53 +0200

    Reformat

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -46,19 +46,19 @@ M.config = {
 
 		appearance = {
 
-				highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- Will be removed in a future release
-				use_nvim_cmp_as_default = false,
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
-				nerd_font_variant = "mono",
-				kind_icons = {
-					Text = "󰉿",
-					Method = "󰊕",
-					Function = "󰊕",
-					Constructor = "󰒓",
+			highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
+			use_nvim_cmp_as_default = false,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+			kind_icons = {
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
 
 				Field = "󰜢",
 				Variable = "󰆦",
