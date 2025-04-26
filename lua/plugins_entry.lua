cb4cc170bfeb94c5786d90b5b60061580d3ea193
Herob527 <szymon.wrzos@onet.pl>
2025-04-26 15:16:26 +0200

    Copied style and added border

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -150,6 +150,32 @@ local lazyinstalls = {
 				temperature = 0.5,
 				max_tokens = 256,
 			},
+			windows = {
+				---@type "right" | "left" | "top" | "bottom"
+				position = "right", -- the position of the sidebar
+				wrap = true, -- similar to vim.o.wrap
+				width = 30, -- default % based on available width
+				sidebar_header = {
+					enabled = true, -- true, false to enable/disable the header
+					align = "center", -- left, center, right for title
+					rounded = true,
+				},
+				input = {
+					prefix = "> ",
+					height = 8, -- Height of the input window in vertical layout
+				},
+				edit = {
+					border = "single",
+					start_insert = true, -- Start insert mode when opening the edit window
+				},
+				ask = {
+					floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+					start_insert = true, -- Start insert mode when opening the ask window
+					border = "single",
+					---@type "ours" | "theirs"
+					focus_on_apply = "ours", -- which diff to focus after applying
+				},
+			},
 		},
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
