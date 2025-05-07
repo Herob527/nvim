f3130ba899bb9c1151e16ffc25cccc4107c168aa
Herob527 <szymon.wrzos@onet.pl>
2025-05-07 13:47:13 +0200

    Remove lsp-lens

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -83,35 +83,6 @@ local lazyinstalls = {
 		event = "VeryLazy",
 	},
 	{
-		"VidocqH/lsp-lens.nvim",
-		event = "BufRead",
-		opts = {
-			include_declaration = true, -- Reference include declaration
-		},
-		sections = {
-			definition = function(count)
-				return "Definitions: " .. count
-			end,
-			references = function(count)
-				return "References: " .. count
-			end,
-			implements = function(count)
-				return "Implements: " .. count
-			end,
-			git_authors = function(latest_author, count)
-				return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-			end,
-		},
-		keys = {
-			{
-				-- LspLensToggle
-				"<leader>uL",
-				"<cmd>LspLensToggle<CR>",
-				desc = "LSP Len Toggle",
-			},
-		},
-	},
-	{
 		"jinh0/eyeliner.nvim",
 		event = "VeryLazy",
 		config = function()
