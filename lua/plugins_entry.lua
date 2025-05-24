0aebecde90b95fa0195497a5486e582aab945b65
Herob527 <szymon.wrzos@onet.pl>
2025-05-24 19:06:00 +0200

    Changed transparency of rainbowlaine

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -135,7 +135,9 @@ local lazyinstalls = {
 		event = "BufReadPost",
 		opts = function(_, opts)
 			-- Other blankline configuration here
-			return require("indent-rainbowline").make_opts(opts)
+			return require("indent-rainbowline").make_opts(opts, {
+				color_transparency = 0.05,
+			})
 		end,
 		dependencies = {
 			"TheGLander/indent-rainbowline.nvim",
