e15dbf965cf9935f8d9ef02561acc147ecbedc2c
Herob527 <szymon.wrzos@onet.pl>
2025-06-03 17:22:02 +0200

    Changed keybinding

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -114,7 +114,7 @@ local lazyinstalls = {
 		config = function()
 			local neocodeium = require("neocodeium")
 			neocodeium.setup()
-			vim.keymap.set("i", "<C-Tab>", neocodeium.accept)
+			vim.keymap.set("i", "<C-q>", neocodeium.accept)
 			vim.keymap.set("i", "<A-e>", function()
 				neocodeium.cycle_or_complete()
 			end)
