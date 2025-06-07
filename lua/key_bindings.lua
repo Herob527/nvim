d5af169e6264299e4e123e2c9ad2db3a54efe58a
Herob527 <szymon.wrzos@onet.pl>
2025-06-07 21:44:13 +0200

    Fixed LspRestart on new neovim

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -1,6 +1,7 @@
 vim.keymap.set({ "n" }, "<leader>ha", function()
 	vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
 end, { desc = "Trigger [h]over [a]ction" })
+
 vim.keymap.set({ "n", "v" }, "<leader>,", function()
 	require("conform").format({ async = true })
 end, { desc = "Trigger [L]SP formatting" })
@@ -9,6 +10,8 @@ vim.keymap.set({ "n" }, "<leader>ef", function()
 	if not vim.fn.exists(":VtsExec") == 2 then
 		vim.cmd("silent exec ':VtsExec restart_tsserver'")
 	end
-	vim.cmd("silent exec ':LspRestart'")
+	vim.iter(vim.lsp.get_clients()):each(function(client)
+		vim.cmd("silent exec ':LspRestart " .. client.name .. "'")
+	end)
 	vim.diagnostic.reset()
 end, { desc = "Reset LSP and diagnostics", silent = true })
