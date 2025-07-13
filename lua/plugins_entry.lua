c99be0c8825a601fa0572bb53fa9360f2c00f9a8
Herob527 <szymon.wrzos@onet.pl>
2025-07-13 17:02:20 +0200

    Adjustment for tmux and zellij

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -114,7 +114,16 @@ local lazyinstalls = {
 		config = function()
 			local neocodeium = require("neocodeium")
 			neocodeium.setup()
-			local accept_keybind = os.getenv("WEZTERM_UNIX_SOCKET") == nil and "<C-Tab>" or "<C-q>"
+			local is_tmux = os.getenv("TERM_PROGRAM") == "tmux"
+			local is_zellij = os.getenv("ZELLIJ") ~= nil
+			local accept_keybind = ""
+			if is_tmux then
+				accept_keybind = "<A-r>"
+			elseif is_zellij then
+				accept_keybind = "<C-Tab>"
+			else
+				accept_keybind = "<C-`>"
+			end
 			vim.keymap.set("i", accept_keybind, neocodeium.accept)
 			vim.keymap.set("i", "<A-e>", function()
 				neocodeium.cycle_or_complete()
