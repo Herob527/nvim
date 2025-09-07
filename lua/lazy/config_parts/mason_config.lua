f35ed05b9ca9dab690722961946f54cdd35f8ed1
Herob527 <szymon.wrzos@onet.pl>
2025-09-07 21:25:14 +0200

    Added registires

--- lua/lazy/config_parts/mason_config.lua
+++ lua/lazy/config_parts/mason_config.lua
@@ -3,6 +3,10 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("mason").setup({
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
+			},
 			ui = {
 				border = "rounded",
 			},
