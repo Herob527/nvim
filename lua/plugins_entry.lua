a19623c4b27f56df7299c94ff0e3ea15471d7568
Herob527 <szymon.wrzos@onet.pl>
2025-09-07 21:25:01 +0200

    Added roslyn

--- lua/plugins_entry.lua
+++ lua/plugins_entry.lua
@@ -193,6 +193,31 @@ local lazyinstalls = {
 			{ "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
 		},
 	},
+	{
+		"seblyng/roslyn.nvim",
+		event = "VeryLazy",
+		---@module 'roslyn.config'
+		---@type RoslynNvimConfig
+		config = function()
+			vim.lsp.config("roslyn", {
+				on_attach = function()
+					print("This will run when the server attaches!")
+				end,
+				settings = {
+					["csharp|inlay_hints"] = {
+						csharp_enable_inlay_hints_for_implicit_object_creation = true,
+						csharp_enable_inlay_hints_for_implicit_variable_types = true,
+					},
+					["csharp|code_lens"] = {
+						dotnet_enable_references_code_lens = true,
+					},
+				},
+			})
+		end,
+		opts = {
+			-- your configuration comes here; leave empty for default settings
+		},
+	},
 }
 
 local opts = {
