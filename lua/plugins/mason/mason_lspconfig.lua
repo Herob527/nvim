60ab6856acd4b272110289a5f93f5346b7ae412f
Herob527 <szymon.wrzos@onet.pl>
2025-10-15 12:35:08 +0200

    Remove redundant lsp config iterator since mason seems to handle everything necessary

--- lua/plugins/mason/mason_lspconfig.lua
+++ lua/plugins/mason/mason_lspconfig.lua
@@ -39,37 +39,6 @@ local mason_lspconfig = {
 	},
 }
 
-local lsp_config_iterator = function()
-	local langs_table = require("utils.langs_table").langs_iterator()
-	local languages = {}
-
-	for lang in langs_table do
-		table.insert(languages, { language = lang[1], init = lang[2] })
-	end
-
-	local lang_idx = 1
-	local lsp_idx = 0
-
-	return function()
-		lsp_idx = lsp_idx + 1
-
-		if lsp_idx > #languages[lang_idx].init.lspconfig then
-			lsp_idx = 1
-			lang_idx = lang_idx + 1
-		end
-
-		if lang_idx <= #languages then
-			--- General data about language
-			local current_lang = languages[lang_idx]
-			--- Data about lsp_config only
-			local current_lsp_data = current_lang.init.lspconfig[lsp_idx]
-			return current_lang, current_lsp_data
-		else
-			return nil
-		end
-	end
-end
-
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = { mason_lspconfig, {
@@ -82,33 +51,5 @@ return {
 	build = function()
 		local install_npm_package = require("utils.install_npm_package")
 		install_npm_package("@vue/typescript-plugin")
-	end,
-	config = function()
-		local lsp_config = require("lspconfig")
-
-		local iter = lsp_config_iterator()
-
-		for _, lsp_data in iter do
-			if lsp_data == nil then
-				goto continue
-			end
-			if lsp_data.lsp then
-				local params = {}
-				if lsp_data.settings then
-					params.settings = type(lsp_data.settings) == "function" and lsp_data.settings() or lsp_data.settings
-				end
-				if lsp_data.init_options then
-					params.init_options = lsp_data.init_options
-				end
-				if lsp_data.before_init then
-					params.before_init = lsp_data.before_init
-				end
-				if lsp_data.filetypes then
-					params.filetypes = lsp_data.filetypes
-				end
-				lsp_config[lsp_data.lsp].setup(params)
-			end
-			::continue::
-		end
 	end,
 }
