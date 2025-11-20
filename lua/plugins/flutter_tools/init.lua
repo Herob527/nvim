87950d8f7916100acd4e2d50bc8e3860118d3e2e
Herob527 <szymon.wrzos@onet.pl>
2025-11-20 18:43:21 +0100

    Added keys for flutter tools bound to ofter used commansd

--- lua/plugins/flutter_tools/init.lua
+++ lua/plugins/flutter_tools/init.lua
@@ -3,6 +3,13 @@ local M = {}
 M.config = {
 	"nvim-flutter/flutter-tools.nvim",
 	lazy = false,
+	keys = {
+		{ "<leader>tr", "<cmd>FlutterRun<cr>", desc = "Flu[t]ter [r]un" },
+		{ "<leader>tl", "<cmd>FlutterLogToggle<cr>", desc = "Flu[t]ter [l]og" },
+		{ "<leader>tcl", "<cmd>FlutterLogClear<cr>", desc = "Flu[t]ter [c]lear [l]og" },
+		{ "<leader>tR", "<cmd>FlutterRestart<cr>", desc = "Flu[t]ter [R]estart" },
+		{ "<leader>tq", "<cmd>FlutterQuit<cr>", desc = "Flu[t]ter [q]uit" },
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
