706ef5979b93c837c593cf8ed32d15a3304a29ca
Herob527 <szymon.wrzos@onet.pl>
2026-01-22 11:54:36 +0100

    Remove lspking usage

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -190,17 +190,10 @@ M.config = {
 					components = {
 						kind_icon = {
 							text = function(ctx)
-								local lspkind = require("lspkind")
 								local icon = ctx.kind_icon
-								if vim.tbl_contains({ "Path" }, ctx.source_name) then
-									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-									if dev_icon then
-										icon = dev_icon
-									end
-								else
-									icon = lspkind.symbolic(ctx.kind, {
-										mode = "symbol",
-									})
+								local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+								if dev_icon then
+									icon = dev_icon
 								end
 
 								return icon .. ctx.icon_gap
