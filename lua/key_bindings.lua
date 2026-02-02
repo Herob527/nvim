52b7c43621509504060667aee271bd2a29941eb4
Herob527 <szymon.wrzos@onet.pl>
2026-02-02 10:24:10 +0100

    Modify restart command

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -2,6 +2,17 @@ vim.keymap.set({ "n" }, "<leader>ha", function()
 	vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
 end, { desc = "Trigger [h]over [a]ction" })
 
+local restart_command = function()
+	local nvim_version = vim.version.parse(vim.system({ "nvim", "-v" }):wait().stdout)
+	if vim.version.lt(nvim_version, { 0, 12, 0 }) then
+		return ":LspRestart"
+	else
+		return ":lsp restart"
+	end
+end
+
+local lsp_restart_command = restart_command()
+
 vim.keymap.set({ "n", "v" }, "<leader>,", function()
 	require("conform").format({ async = true })
 end, { desc = "Trigger [L]SP formatting" })
@@ -16,7 +27,7 @@ vim.keymap.set({ "n" }, "<leader>ef", function()
 			return client.name ~= "vtsls"
 		end)
 		:each(function(client)
-			vim.cmd("silent exec ':lsp restart " .. client.name .. "'")
+			vim.cmd("silent exec '" .. lsp_restart_command .. " " .. client.name .. "'")
 		end)
 	vim.diagnostic.reset()
 end, { desc = "Reset LSP and diagnostics", silent = true })
