099e45ed176f83b339972de9b7c36198128d1ee9
Herob527 <szymon.wrzos@onet.pl>
2026-03-11 13:45:07 +0100

    Switch to easy dotnet

--- lua/plugins/roslyn/init.lua
+++ lua/plugins/roslyn/init.lua
@@ -1,29 +1,12 @@
 local M = {}
 
 M.config = {
-	"seblyng/roslyn.nvim",
+	"GustavEikaas/easy-dotnet.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	event = "VeryLazy",
-	---@module 'roslyn.config'
-	---@type RoslynNvimConfig
 	config = function()
-		vim.lsp.config("roslyn", {
-			on_attach = function()
-				print("This will run when the server attaches!")
-			end,
-			settings = {
-				["csharp|inlay_hints"] = {
-					csharp_enable_inlay_hints_for_implicit_object_creation = true,
-					csharp_enable_inlay_hints_for_implicit_variable_types = true,
-				},
-				["csharp|code_lens"] = {
-					dotnet_enable_references_code_lens = true,
-				},
-			},
-		})
+		require("easy-dotnet").setup()
 	end,
-	opts = {
-		-- your configuration comes here; leave empty for default settings
-	},
 }
 
 return M
