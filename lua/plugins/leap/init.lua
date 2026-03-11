b7d4ce5d407914aa659e7f66326860fed8629a4c
Herob527 <szymon.wrzos@onet.pl>
2026-03-11 13:45:00 +0100

    Update mappings

--- lua/plugins/leap/init.lua
+++ lua/plugins/leap/init.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 M.init = function()
-	require("leap").add_default_mappings()
+	-- require("leap").add_default_mappings()
+	vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+	vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
 end
 
 M.config = {
